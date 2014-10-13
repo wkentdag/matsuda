7630ddc7c3b741bfac5828e8770e8ed355d6d5aa
Will Kent-Daggett <willkentdaggett@gmail.com>
2014-10-12 21:07:52 -0500

    finished w/layout i think

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,18 +1,35 @@
 $(document).ready(function () {
 	$('#fullpage').fullpage({
+		anchors: pages,
 		loopBottom: true,
 		loopHorizontal: true,
 		menu: '#overlay',
-		afterSlideLoad: function(anchorLink, slideAnchor) {
-			console.log('=======');
-			console.log(anchorLink, slideAnchor);
-			if (anchorLink === 'about') {
-				console.log(slideAnchor + ' if!!!');
+		resize: false,
+		afterLoad: function(anchorLink, index) {
+			for (var i=0; i<pages.length; i++) {
+				var page = pages[i];
+				console.log(page, anchorLink);
+				if (anchorLink === page) {
+					// document.location.hash = page;
+					if (page === '#') {
+						var selector = '#home'
+					} else {
+						var selector = "a[href$=" + '"' + page + '"' + "]";
+					}
+					console.log(selector);
+					currentPage = selector;
+					$(selector).addClass('hover');
+				}
 			}
+		},
+		onLeave: function(index, nextIndex) {
+			$(currentPage).removeClass('hover');
 		}
 	});
 
-	
-
-
+	$.fn.fullpage.setScrollingSpeed(500);	
 });
+
+var pages = ['#', 'about', 'wedding', 'landscape', 'portraits'];
+
+var currentPage;
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'William Matsuda' });
+  res.render('index', { title: 'Will Matsuda' });
 });
 
 module.exports = router;
