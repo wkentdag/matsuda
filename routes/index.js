df3e4856acf5eea9d6714f4be46f02499d010167
Will Kent-Daggett <willkentdaggett@gmail.com>
2014-10-12 23:26:40 -0500

    talking w tumblr

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
+var tumblr = require('tumblr.js');
+var client = require('../tumblr-auth.js')
+
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Will Matsuda' });
+	client.posts('williammatsuda', {type: 'photo', limit: 10}, function (err, data) {
+		if (err) {console.log(err);} else {
+			console.log(data);
+			// for (var p in data.posts) {
+			// 	console.log(p.photos.[0].caption);
+			// }
+
+			for (var i=0; i<10; i++) {
+				console.log(data.posts[i].photos[0].alt_sizes[0].url)
+			}
+		}
+
+		res.render('index', { title: 'Will Matsuda' });
+	});
 });
 
 module.exports = router;
