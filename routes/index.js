b9580ddb10f966219dc754a6c9a2167bc80fc617
Will Kent-Daggett <willkentdaggett@gmail.com>
2014-10-13 00:00:43 -0500

    catching all images by tag

--- routes/index.js
+++ routes/index.js
@@ -6,20 +6,77 @@ var client = require('../tumblr-auth.js')
 
 /* GET home page. */
 router.get('/', function(req, res) {
-	client.posts('williammatsuda', {type: 'photo', limit: 10}, function (err, data) {
-		if (err) {console.log(err);} else {
-			console.log(data);
-			// for (var p in data.posts) {
-			// 	console.log(p.photos.[0].caption);
-			// }
+	var lovers = [];
+	var fashion = [];
+	var everyday = [];
+	var video = [];
+	var mpr = [];
+	var globalpost = [];
 
-			for (var i=0; i<10; i++) {
-				console.log(data.posts[i].photos[0].alt_sizes[0].url)
+	client.posts('williammatsuda', {type: 'photo', tag: 'lovers', limit: 10}, function (err, data) {
+		if (err) {console.log(err);} else {
+			for (var p in data.posts) {
+				// console.log(p);
+				// console.log(data.posts[p].photos[0].alt_sizes[0].url);
+				lovers.push(data.posts[p].photos[0].alt_sizes[0].url);
 			}
+			// console.log('lovers: ====');
+			// console.log(lovers);
+			client.posts('williammatsuda', {type: 'photo', tag: 'fashion', limit: 10}, function (err, data) {
+				if (err) {console.log(err);} else {
+					for (var p in data.posts) {
+						fashion.push(data.posts[p].photos[0].alt_sizes[0].url);
+					}
+					// console.log('fashion: ============');
+					// console.log(fashion);
+					client.posts('williammatsuda', {type: 'photo', tag: 'everyday', limit: 10}, function (err, data) {
+						if (err) {console.log(err);} else {
+							for (var p in data.posts) {
+								everyday.push(data.posts[p].photos[0].alt_sizes[0].url);
+							}
+							// console.log('everyday1111111');
+							// console.log(everyday);
+
+							client.posts('williammatsuda', {type: 'photo', tag: 'Minnesota Public Radio', limit: 10}, function (err, data) {
+								if (err) {console.log(err);} else {
+									for (var p in data.posts) {
+										mpr.push(data.posts[p].photos[0].alt_sizes[0].url);
+									}
+									// console.log('mpr: ');
+									// console.log(mpr);
+									client.posts('williammatsuda', {type: 'photo', tag: 'The Global Post', limit: 10},
+										function (err, data) {
+											if (err) {console.log(err);} else {
+												for (var p in data.posts) {
+													globalpost.push(data.posts[p].photos[0].alt_sizes[0].url);
+												}
+
+												// console.log('global post: ')
+												// console.log(globalpost);
+
+												var images = [];
+												images.lovers = lovers;
+												images.fashion = fashion;
+												images.everyday = everyday;
+												images.mpr = mpr;
+												images.gp = globalpost;
+
+												// console.log(images);
+												res.render('index', {
+													title: 'Will Matsuda',
+													images: images
+												});
+											}
+										}); //	end gp get
+								}
+							});	//	end mpr get
+						}
+					});	//	end everyday get
+				}
+			});	//	end fashion get
 		}
 
-		res.render('index', { title: 'Will Matsuda' });
-	});
+	});	//	end lovers get
 });
 
 module.exports = router;
