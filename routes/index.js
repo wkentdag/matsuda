2b0ffc627da7dce8ef309c13eeace57e523f24a1
Will Kent-Daggett <willkentdaggett@gmail.com>
2015-03-08 16:15:41 -0500

    properly parsing categories.json

--- routes/index.js
+++ routes/index.js
@@ -11,12 +11,23 @@ var async = require('async');
 router.get('/', function(req, res) {
 
 	var categories = JSON.parse(fs.readFileSync('public/files/categories.json', 'utf8'));
+	var tags = categories.tags;
+	
+	for (item in tags) {
+		console.log(tags[item].tag);
+	}
 
-	client.getPosts('lovers', 'photo', 10, function(err, posts) {
-		console.log(posts);
+	// forEachAsync(categories.tags, function (next, cat, i, arr) {
+	// 	console.log('hi');
+	// 	next();
+	// }).then(function () {	
+	// 	console.log('all done!')
+		client.getPosts('lovers', 'photo', 10, function(err, posts) {
+			// console.log(posts);
 
-		res.render('index', {title: 'William Matsuda', docs: posts});
-	});
+			res.render('index', {title: 'William Matsuda', docs: posts});
+		});
+	// });
 });
 
 
