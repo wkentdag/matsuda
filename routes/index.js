378aa7ebe42e821b9bda071f2d0a9154744ca38b
Will Kent-Daggett <willkentdaggett@gmail.com>
2015-05-25 16:43:02 -0500

    decouple static text rendering into separate file in lib/

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,17 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
+var text = require('../lib/static-text');
 
-
-
-/* GET home page. */
 router.get('/', function(req, res) {
-	var categories = JSON.parse(fs.readFileSync('public/files/categories.json', 'utf8'));
-	var about = JSON.parse(fs.readFileSync('public/files/landing-text.json', 'utf8'));
-	var published = JSON.parse(fs.readFileSync('public/files/published.json', 'utf8'));
+	console.log('hello from index.js\n' + JSON.stringify(text) );
 
 	res.render('index', {
 		title: 'William Matsuda',
-		categories: categories.categories,
-		about: about.text,
-		published: published.published
+		categories: text.categories,
+		about: text.landing,
+		published: text.published
 	});
 });
-
-
-
 
 module.exports = router;
