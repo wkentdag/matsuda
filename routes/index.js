9e54b571add1a118671cb0d304504c6538ec4d5f
Will Kent-Daggett <willkentdaggett@gmail.com>
2015-05-29 16:00:06 -0500

    messing w styling

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,14 @@ var router = express.Router();
 var text = require('../lib/static-text');
 
 router.get('/', function(req, res) {
+  console.log(text.tags);
 	res.render('index', {
 		title: 'William Matsuda',
 		categories: text.categories,
 		about: text.landing,
     contact: text.contact,
-		published: text.published
+		published: text.published,
+    tags: text.tags
 	});
 });
 
