919c4d01be5e18da1294614715a099db5c017504
Will Kent-Daggett <willkentdaggett@gmail.com>
2015-05-31 17:25:17 -0500

    parse sections and galleries in lib/sort-static, pass to index template

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,13 @@ var text = require('../lib/static-text');
 var staticAssets = require('../lib/sort-static');
 
 var sections = staticAssets.sections();
+var galleries = staticAssets.galleries();
 
 router.get('/', function(req, res) {
 	res.render('index', {
 		title: 'William Matsuda',
-		categories: text.categories,
+    sections: sections,
+    galleries: galleries,
 		about: text.landing,
     contact: text.contact,
 		published: text.published,
