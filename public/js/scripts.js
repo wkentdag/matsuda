57bb7c884b53e0d201850744eaed32d0baec04dd
Will Kent-Daggett <willkentdaggett@gmail.com>
2015-06-10 15:16:23 -0700

    loop fullpage, check window size onResize

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,33 @@
 $(document).ready(function() {
-    $('#fullpage').fullpage();
+    $('#fullpage').fullpage({
+      loopBottom: true,
+      loopTop: true,
+      controlArrows: false
+    });
+
+    // $('img').each(function(i) {
+    //   console.log($(this);
+    // });
+
+    $(window).resize(function() {
+      console.log(getWidth() );
+    });
 });
+
+function getWidth() {
+  var width = $(window).width();
+  if (width >= 1200) {
+  } else if (width >= 992) {
+  } else if (width >= 768) {
+  } else {
+  }
+
+  imageSource(width);
+
+  return width;
+}
+
+
+function imageSource(w) {
+  console.log('resizing images to width:\t' + w);
+}
