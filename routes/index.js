60235de2943fbc7b8a8d6bcd8062bc20eae6bcbf
Will Kent-Daggett <willkentdaggett@gmail.com>
2015-06-21 16:32:14 -0500

    parse galleries and sections for jade on request instead of load; add 'traces' category

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,11 @@ var router = express.Router();
 var text = require('../lib/static-text');
 var staticAssets = require('../lib/sort-static');
 
-var sections = staticAssets.sections();
-var galleries = staticAssets.galleries();
-
 router.get('/', function(req, res) {
 	res.render('index', {
 		title: 'William Matsuda',
-    sections: sections,
-    galleries: galleries,
+    sections: staticAssets.sections(),
+    galleries: staticAssets.galleries(),
 		about: text.landing,
     contact: text.contact,
 		published: text.published,
